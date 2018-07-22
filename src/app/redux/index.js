98afa1a04136bea882db9bf0da528f91f3745e8a
Axel Yung <axelyung@gmail.com>
2018-07-22 21:14:30 +0200

    added redux-logger when devtool extension is missing

--- src/app/redux/index.js
+++ src/app/redux/index.js
@@ -1,18 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { init } from 'yarfl';
-
+import logger from 'redux-logger';
 import configs from './configs';
 
 // initialize forms
 const { reducer, initialState, connect, FormProvider } = init(...configs);
 
-// add chrome devtool
-const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// add redux devtool
+const devToolExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// add thunk middleware
-const storeCreator = compose(applyMiddleware(thunk), devTool)(createStore);
+// use redux-logger middleware if devtool extension is missing
+const enhancer = devToolExtension
+    ? compose(applyMiddleware(thunk), devToolExtension)
+    : applyMiddleware(thunk, logger);
 
-const store = storeCreator(reducer, initialState);
+const store = createStore(reducer, initialState, enhancer);
 
 export { connect, FormProvider, store };
